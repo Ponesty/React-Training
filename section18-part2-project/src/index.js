744ef59a4a4b3dcc7ca1f3a2189e30e6d143e8fe
Jerrell <jerrellprier@gmail.com>
2022-03-15 11:38:36 -0500

    Fixed the provider wrapper in index js in src folder. Imported useSelector in Counter js to use the counter redux in file.

--- section18-part2-project/src/index.js
+++ section18-part2-project/src/index.js
@@ -8,6 +8,7 @@ import store from "./store/index";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />, document.getElementById('root')
-  </Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
