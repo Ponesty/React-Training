bbcef771d715c05759957363c5dae1055c54e700
Jerrell <jerrellprier@gmail.com>
2022-03-15 15:57:08 -0500

    In index js in store folder we exported counterActions. We changed action amount on line 17 to action payload. We then went to Counter js and imported counterActions so we can disoatch the reducers in the handler functions.

--- section18-part2-project/src/components/Counter.js
+++ section18-part2-project/src/components/Counter.js
@@ -1,6 +1,8 @@
 // import { Component } from "react";
 import { useSelector, useDispatch /*, connect*/ } from "react-redux";
 
+import { counterActions } from "../store/index";
+
 import classes from "./Counter.module.css";
 
 const Counter = () => {
@@ -9,19 +11,23 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    // dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 10 });
+    // dispatch({ type: "increase", amount: 10 });
+    dispatch(counterActions.increase(10)); // {type: SOME_UNIQUE_IDENTIFIER, payload: 10}
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    // dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    // dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
