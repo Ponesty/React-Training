10a43d246f1f5f20003aef4b11e447cb13d03308
Jerrell <jerrellprier@gmail.com>
2022-03-16 15:48:49 -0500

    In cart-slice js we added state.totalQuantity ++ & -- to add and remove reducers. In Cart button we use selector to update total qantity in cart badge

--- section19-redux-project/src/store/cart-slice.js
+++ section19-redux-project/src/store/cart-slice.js
@@ -7,6 +7,8 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+
       if (!existingItem) {
         state.items.push({
           itemId: newItem.id,
@@ -23,6 +25,8 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
