65266f392eceae51537fa5541810f13d41531837
Jerrell <jerrellprier@gmail.com>
2022-03-17 11:08:54 -0500

    Added notification const to check if notification state is not null. If not null App js will return the Notification component.

--- section19-redux-project/src/App.js
+++ section19-redux-project/src/App.js
@@ -1,14 +1,18 @@
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { uiActions } from "./store/ui-slice";
 import { useEffect } from "react";
+
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
 
 function App() {
   const dispatch = useDispatch();
   const ui = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
     const sendCartData = async () => {
@@ -53,10 +57,19 @@ function App() {
   }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {ui && <Cart />}
-      <Products />
-    </Layout>
+    <React.Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {ui && <Cart />}
+        <Products />
+      </Layout>
+    </React.Fragment>
   );
 }
 
