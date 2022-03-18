d687b84b6343ecd454e8bb8dae4219c09ca803f0
Jerrell <jerrellprier@gmail.com>
2022-03-18 09:20:19 -0500

    Added a componetns folder. Inside folder we created MainHeader js. Imported MainHeader to App js.

---
+++ section20-multipage-project/src/components/MainHeader.js
@@ -0,0 +1,16 @@
+const MainHeader = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a href="/welcome">Welcome Page</a>
+        </li>
+        <li>
+          <a href="/products">Products Page</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default MainHeader;
