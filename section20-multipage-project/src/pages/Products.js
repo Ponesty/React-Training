ba246aa6f5c08c6f285f4e329f767c8273760023
Jerrell <jerrellprier@gmail.com>
2022-03-18 12:13:06 -0500

    In App js we changed /product-detail to /products. In Product js we added <Link> in <li>

--- section20-multipage-project/src/pages/Products.js
+++ section20-multipage-project/src/pages/Products.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <section>
       <h1>Here we have the Products Page!</h1>
       <ul>
-        <li>10,000 Runes</li>
-        <li>Lightsaber</li>
-        <li>Moldy Burrito</li>
+        <li>
+          <Link to="/products/p1">10,000 Runes</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Lightsaber</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">Moldy Burrito</Link>
+        </li>
       </ul>
     </section>
   );
