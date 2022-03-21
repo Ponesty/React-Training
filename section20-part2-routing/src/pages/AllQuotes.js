0d6e9b47900a4070bcb4c5da174228a384454650
Jerrell <jerrellprier@gmail.com>
2022-03-21 11:46:57 -0500

    Added dummy qutes & imported QuotesList.

--- section20-part2-routing/src/pages/AllQuotes.js
+++ section20-part2-routing/src/pages/AllQuotes.js
@@ -1,5 +1,14 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Eren Jeager", text: "Simp for no one!" },
+  { id: "q1", author: "Vinsmoke Sanji", text: "Keep the simp gang strong!" },
+  { id: "q1", author: "Maidenless Tarnish", text: "I'm the Elden Lord!" },
+  { id: "q1", author: "Saskue Uchiha", text: "I will become Hokage, no cap!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList />;
 };
 
 export default AllQuotes;
