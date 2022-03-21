18d2ee1bf22a7b053859ceaf8865ffbb34f163e6
Jerrell <jerrellprier@gmail.com>
2022-03-21 11:49:53 -0500

    Added props to <QuoteList> with DUMMY_QUOTES

--- section20-part2-routing/src/pages/AllQuotes.js
+++ section20-part2-routing/src/pages/AllQuotes.js
@@ -2,13 +2,13 @@ import QuoteList from "../components/quotes/QuoteList";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Eren Jeager", text: "Simp for no one!" },
-  { id: "q1", author: "Vinsmoke Sanji", text: "Keep the simp gang strong!" },
-  { id: "q1", author: "Maidenless Tarnish", text: "I'm the Elden Lord!" },
-  { id: "q1", author: "Saskue Uchiha", text: "I will become Hokage, no cap!" },
+  { id: "q2", author: "Vinsmoke Sanji", text: "Keep the simp gang strong!" },
+  { id: "q3", author: "Maidenless Tarnish", text: "I'm the Elden Lord!" },
+  { id: "q4", author: "Saskue Uchiha", text: "I will become Hokage, no cap!" },
 ];
 
 const AllQuotes = () => {
-  return <QuoteList />;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
