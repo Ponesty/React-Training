5e33a3188284363404fbe784bcd38dff60b1c989
Jerrell <jerrellprier@gmail.com>
2022-03-21 12:20:09 -0500

    Temporarily added DUMMY_QUOTES. Will get quotes from server later.

--- section20-part2-routing/src/pages/QuoteDetail.js
+++ section20-part2-routing/src/pages/QuoteDetail.js
@@ -2,6 +2,13 @@ import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Eren Jeager", text: "Simp for no one!" },
+  { id: "q2", author: "Vinsmoke Sanji", text: "Keep the simp gang strong!" },
+  { id: "q3", author: "Maidenless Tarnish", text: "I'm the Elden Lord!" },
+  { id: "q4", author: "Saskue Uchiha", text: "I will become Hokage, no cap!" },
+];
+
 const QuoteDetail = () => {
   const params = useParams();
   return (
