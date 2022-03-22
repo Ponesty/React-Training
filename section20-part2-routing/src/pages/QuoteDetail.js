ca84c684ed6513ac8c44d5841ce72f007312be4a
Jerrell <jerrellprier@gmail.com>
2022-03-22 12:29:22 -0500

    Got Detail page to get quotes from server.

--- section20-part2-routing/src/pages/QuoteDetail.js
+++ section20-part2-routing/src/pages/QuoteDetail.js
@@ -1,29 +1,61 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Eren Jeager", text: "Simp for no one!" },
-  { id: "q2", author: "Vinsmoke Sanji", text: "Keep the simp gang strong!" },
-  { id: "q3", author: "Maidenless Tarnish", text: "I'm the Elden Lord!" },
-  { id: "q4", author: "Saskue Uchiha", text: "I will become Hokage, no cap!" },
-];
+// const DUMMY_QUOTES = [
+//   { id: "q1", author: "Eren Jeager", text: "Simp for no one!" },
+//   { id: "q2", author: "Vinsmoke Sanji", text: "Keep the simp gang strong!" },
+//   { id: "q3", author: "Maidenless Tarnish", text: "I'm the Elden Lord!" },
+//   { id: "q4", author: "Saskue Uchiha", text: "I will become Hokage, no cap!" },
+// ];
 
 const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params;
 
-  if (!quote) {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  // const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
+  // if (!quote) {
+  //   return <p>No quote found!</p>;
+  // }
+
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
