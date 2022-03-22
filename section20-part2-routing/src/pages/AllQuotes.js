22f03ceaee47fe22ea01e7b28f7a1e5bf5b72d5c
Jerrell <jerrellprier@gmail.com>
2022-03-22 12:13:30 -0500

    Getting all quotes fom server.

--- section20-part2-routing/src/pages/AllQuotes.js
+++ section20-part2-routing/src/pages/AllQuotes.js
@@ -1,17 +1,48 @@
+import { useEffect } from "react";
+
 import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
 
 import useHttp from "../hooks/use-http";
 import { getAllQuotes } from "../lib/api";
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Eren Jeager", text: "Simp for no one!" },
-  { id: "q2", author: "Vinsmoke Sanji", text: "Keep the simp gang strong!" },
-  { id: "q3", author: "Maidenless Tarnish", text: "I'm the Elden Lord!" },
-  { id: "q4", author: "Saskue Uchiha", text: "I will become Hokage, no cap!" },
-];
+// const DUMMY_QUOTES = [
+//   { id: "q1", author: "Eren Jeager", text: "Simp for no one!" },
+//   { id: "q2", author: "Vinsmoke Sanji", text: "Keep the simp gang strong!" },
+//   { id: "q3", author: "Maidenless Tarnish", text: "I'm the Elden Lord!" },
+//   { id: "q4", author: "Saskue Uchiha", text: "I will become Hokage, no cap!" },
+// ];
 
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES} />;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
