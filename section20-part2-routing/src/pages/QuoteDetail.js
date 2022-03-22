07dd345fe7717bffd035e301543a4bce1a205eac
Jerrell <jerrellprier@gmail.com>
2022-03-22 11:12:50 -0500

    Added a <div> with <Link> inside it to create a load comments button for the quote detail page

--- section20-part2-routing/src/pages/QuoteDetail.js
+++ section20-part2-routing/src/pages/QuoteDetail.js
@@ -3,6 +3,7 @@ import { useParams, Route } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+import { Link } from "react-router-dom";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Eren Jeager", text: "Simp for no one!" },
@@ -23,6 +24,11 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <div className="centered">
+        <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          Load Comments
+        </Link>
+      </div>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
