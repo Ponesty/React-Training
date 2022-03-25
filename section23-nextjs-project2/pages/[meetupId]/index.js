c7d7fc28533b9dff55c38ec5d91b12432d0080cf
Jerrell <jerrellprier@gmail.com>
2022-03-25 11:45:51 -0500

    In index, we added <img>,<h1>,<address>,<p>. We then created the MeetupDetail.js

--- section23-nextjs-project2/pages/[meetupId]/index.js
+++ section23-nextjs-project2/pages/[meetupId]/index.js
@@ -1,7 +1,17 @@
 import { Fragment } from "react";
 
 const MeetupDetails = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <img
+        src="https://globalgrasshopper.com/wp-content/uploads/2020/03/most-beautiful-places-to-visit-in-Japan-1000x667.jpg"
+        alt="First"
+      />
+      <h1>First Meetup</h1>
+      <address>Address 1, A Town</address>
+      <p>Meet up baw!</p>
+    </Fragment>
+  );
 };
 
 export default MeetupDetails;
