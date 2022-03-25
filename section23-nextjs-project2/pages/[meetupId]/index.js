9842e28d54ed08e73e3cf2ee72cd6146a67bdff1
Jerrell <jerrellprier@gmail.com>
2022-03-25 14:43:17 -0500

    Added meetupId to getStaticProps and created the getStaticPatrhs function.

--- section23-nextjs-project2/pages/[meetupId]/index.js
+++ section23-nextjs-project2/pages/[meetupId]/index.js
@@ -11,14 +11,19 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {};
+
 export const getStaticProps = async (context) => {
   // fetch data from a single meetup
+
+  const meetupId = context.params.meetupId;
+
   return {
     props: {
       meetupData: {
         image:
           "https://globalgrasshopper.com/wp-content/uploads/2020/03/most-beautiful-places-to-visit-in-Japan-1000x667.jpg",
-        id: "m1",
+        id: meetupId,
         title: "First Meetup",
         address: "Address 001, 12345 A Town",
         description: "First meetup!",
