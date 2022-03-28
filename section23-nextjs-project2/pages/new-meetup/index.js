5b3b2cb5ee30c2b3ea10ddf4d74de00ef0985ecc
Jerrell <jerrellprier@gmail.com>
2022-03-28 13:52:09 -0500

    Added useRouter to be sent back to / page.

--- section23-nextjs-project2/pages/new-meetup/index.js
+++ section23-nextjs-project2/pages/new-meetup/index.js
@@ -1,8 +1,11 @@
+import { useRouter } from "next/router";
 // our-domain.com/new-meetup
 
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
+  const router = useRouter();
+
   const addMeetupHandler = async (enteredMeetupData) => {
     const response = await fetch("/api/new-meetup", {
       method: "POST",
@@ -14,7 +17,9 @@ const NewMeetupPage = () => {
 
     const data = await response.json();
     console.log(data);
+    router.push("/");
   };
+
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
 
