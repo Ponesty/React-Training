26a4bfff93e9b139869f02b3b3197539d38e2cc3
Jerrell <jerrellprier@gmail.com>
2022-04-04 11:02:59 -0500

    In App we added todos array as props for Todos component. In Todos we chnaged the prop type from an array of strings to an array of Todo. Then we added item.id to key and item.text for list.

--- section27-typescript-project/react-ts/src/App.tsx
+++ section27-typescript-project/react-ts/src/App.tsx
@@ -2,10 +2,10 @@ import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TyoeScript")];
+  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
   return (
     <div>
-      <Todos items={["Learn React", "Learn TypeScript"]} />
+      <Todos items={todos} />
     </div>
   );
 }
