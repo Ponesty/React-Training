712004cd4d0e3ff1af09178a06e6340ad9b9babc
Jerrell <jerrellprier@gmail.com>
2022-04-04 14:08:35 -0500

    Removed hard coded Todos in array from usestate. Replace with empty array and gave useState a Generic of type Todo[]

--- section27-typescript-project/react-ts/src/App.tsx
+++ section27-typescript-project/react-ts/src/App.tsx
@@ -4,10 +4,7 @@ import Todo from "./models/todo";
 import NewTodo from "./components/NewTodo";
 
 function App() {
-  const [todos, setTodos] = useState([
-    new Todo("Learn React"),
-    new Todo("Learn TypeScript"),
-  ]);
+  const [todos, setTodos] = useState<Todo[]>([]);
   const addTodo = (todoText: string) => {
     setTodos([...todos, new Todo(todoText)]);
   };
