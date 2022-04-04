d8670423e0bf3e4e79c379ded86ea4fb4b941f34
Jerrell <jerrellprier@gmail.com>
2022-04-04 14:41:55 -0500

    Using styling in each tsx file.

--- section27-typescript-project/react-ts/src/components/NewTodo.tsx
+++ section27-typescript-project/react-ts/src/components/NewTodo.tsx
@@ -13,7 +13,7 @@ const NewTodo: React.FC<{ onAddTodo: (todo: string) => void }> = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <label htmlFor="text">Todo Text</label>
       <input type="text" id="text" ref={todoInputRef} />
       <button>Add Todo</button>
