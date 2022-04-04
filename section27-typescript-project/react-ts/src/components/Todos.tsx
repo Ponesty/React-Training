28af13be5c456a7aeec67d5914a74903fdea1ba2
Jerrell <jerrellprier@gmail.com>
2022-04-04 16:58:08 -0500

    Added useContext and replaced all props items with todoCtx

--- section27-typescript-project/react-ts/src/components/Todos.tsx
+++ section27-typescript-project/react-ts/src/components/Todos.tsx
@@ -1,21 +1,20 @@
-import React from "react";
-import Todo from "../models/todo";
+import React, { useContext } from "react";
+
+import { TodosContext } from "../store/todo-context";
 import TodoItem from "./TodoItem";
 
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{
-  items: Todo[];
-  onDelete: (todoId: string) => void;
-}> = (props) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
+      {todosCtx.items.map((item) => (
         <TodoItem
           key={item.id}
           id={item.id}
           text={item.text}
-          deleteTodo={props.onDelete}
+          deleteTodo={todosCtx.deleteTodo}
         />
       ))}
     </ul>
