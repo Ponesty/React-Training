e51306266ff507d50a326bc656a2984707f03005
Jerrell <jerrellprier@gmail.com>
2022-04-04 16:39:24 -0500

    Added context provider and put cype in TodosContextObj param.

--- section27-typescript-project/react-ts/src/store/todo-context.tsx
+++ section27-typescript-project/react-ts/src/store/todo-context.tsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "../models/todo";
 
-const TodosContext = React.createContext<{
+type TodosContextObj = {
   items: Todo[];
   addTodo: () => void;
   deleteTodo: (id: string) => void;
-}>({
+};
+
+const TodosContext = React.createContext<TodosContextObj>({
   items: [],
   addTodo: () => {},
   deleteTodo: (id: string) => {},
 });
+
+const TodosContextProvider: React.FC = (props) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addTodo = (todoText: string) => {
+    // setTodos([...todos, new Todo(todoText)]); How I did it. It works the same.
+
+    // How instructor did it.
+    setTodos((prevTodos) => {
+      return prevTodos.concat(new Todo(todoText));
+    });
+  };
+
+  const deleteTodo = (todoId: string) => {
+    setTodos(
+      todos.filter((todo) => {
+        return todo.id !== todoId;
+      })
+    );
+  };
+
+  const contextValue: TodosContextObj = {
+    items: todos,
+    addTodo: addTodo,
+    deleteTodo: deleteTodo,
+  };
+  return (
+    <TodosContextProvider value={contextValue}>
+      {props.children}
+    </TodosContextProvider>
+  );
+};
