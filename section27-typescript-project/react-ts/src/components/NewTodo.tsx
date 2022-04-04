0f67ff9203d7e8967c9e0e2b0133e9551838f3c5
Jerrell <jerrellprier@gmail.com>
2022-04-04 12:08:47 -0500

    Added useRef to form. useRef needed HTMLInputElement as a generic so it will know the type it will get.

--- section27-typescript-project/react-ts/src/components/NewTodo.tsx
+++ section27-typescript-project/react-ts/src/components/NewTodo.tsx
@@ -1,13 +1,14 @@
 import React, { useRef } from "react";
 
 const newTodo = () => {
+  const todoInputRef = useRef<HTMLInputElement>(null);
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
   };
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo Text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoInputRef} />
       <button>Add Todo</button>
     </form>
   );
