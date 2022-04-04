6b1733de8df4e88ba25fc15dc2294a387bd2415c
Jerrell <jerrellprier@gmail.com>
2022-04-04 16:48:14 -0500

    Removed code that is now in todo-context.

--- section27-typescript-project/react-ts/src/App.tsx
+++ section27-typescript-project/react-ts/src/App.tsx
@@ -4,24 +4,6 @@ import Todo from "./models/todo";
 import NewTodo from "./components/NewTodo";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const addTodo = (todoText: string) => {
-    // setTodos([...todos, new Todo(todoText)]); How I did it. It works the same.
-
-    // How instructor did it.
-    setTodos((prevTodos) => {
-      return prevTodos.concat(new Todo(todoText));
-    });
-  };
-
-  const deleteTodo = (todoId: string) => {
-    setTodos(
-      todos.filter((todo) => {
-        return todo.id !== todoId;
-      })
-    );
-  };
-
   return (
     <Fragment>
       <NewTodo onAddTodo={addTodo} />
