e65a5b9e751289af0d8612c943e1830bc58b4d3a
Jerrell <jerrellprier@gmail.com>
2022-04-04 10:57:34 -0500

    Imported Todos class and added 2 new Todos in the todos array.

--- section27-typescript-project/react-ts/src/App.tsx
+++ section27-typescript-project/react-ts/src/App.tsx
@@ -1,6 +1,8 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn TyoeScript")];
   return (
     <div>
       <Todos items={["Learn React", "Learn TypeScript"]} />
