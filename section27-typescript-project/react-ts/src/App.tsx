99aa8c023ea1d08d01bcf4edc0bab95781299c7a
Jerrell <jerrellprier@gmail.com>
2022-04-04 12:18:18 -0500

    Replaced <div> with <Fragment>

--- section27-typescript-project/react-ts/src/App.tsx
+++ section27-typescript-project/react-ts/src/App.tsx
@@ -1,12 +1,13 @@
+import { Fragment } from "react";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
   const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
   return (
-    <div>
+    <Fragment>
       <Todos items={todos} />
-    </div>
+    </Fragment>
   );
 }
 
