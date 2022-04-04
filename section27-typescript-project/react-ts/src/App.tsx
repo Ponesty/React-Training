19169ad48171e8e45d932db4b2d5d60124b38375
Jerrell <jerrellprier@gmail.com>
2022-04-04 12:52:03 -0500

    In NewTodo we used React.FC for props. In App we added <NewTodo> with a function for props and used useState for todos array.

--- section27-typescript-project/react-ts/src/App.tsx
+++ section27-typescript-project/react-ts/src/App.tsx
@@ -1,11 +1,19 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  const [todos, setTodos] = useState([
+    new Todo("Learn React"),
+    new Todo("Learn TypeScript"),
+  ]);
+  const addTodo = (todo: string) => {
+    setTodos([...todos, new Todo(todo)]);
+  };
   return (
     <Fragment>
+      <NewTodo onAddTodo={addTodo} />
       <Todos items={todos} />
     </Fragment>
   );
--- section27-typescript-project/react-ts/src/components/NewTodo.tsx
+++ section27-typescript-project/react-ts/src/components/NewTodo.tsx
@@ -1,12 +1,15 @@
 import React, { useRef } from "react";
 
-const newTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: any }> = (props) => {
   const todoInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const enteredText = todoInputRef.current?.value;
     console.log(enteredText);
+    props.onAddTodo(enteredText);
   };
+
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo Text</label>
@@ -16,4 +19,4 @@ const newTodo = () => {
   );
 };
 
-export default newTodo;
+export default NewTodo;
